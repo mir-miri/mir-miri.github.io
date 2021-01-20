ea4d998800ec4171b102e0feba3cca97d894f0b1
mir-miri <76711137+mir-miri@users.noreply.github.com>
2021-01-20 22:48:10 +0000

    Add files via upload

--- okijdfvh.js
+++ okijdfvh.js
@@ -47,12 +47,12 @@
 				},],
 				hash: "eizQCoZ-zF3-woGNXWyr6gL1CAoqeiLa"
 			};
-			e.exports = l
+			e.exports=l;
 		},
 		"3Mmi": function(e, n, a) {
 			(window.__NEXT_P = window.__NEXT_P || []).push(["/poop", function() {
-				return a("dmPs")
-			}])
+				return a("dmPs");
+			}]);
 		},
 		"4Us4": function(e, n, a) {
 			"use strict";
@@ -72,7 +72,7 @@
 					cursor: "pointer",
 					outline: "none",
 					"&:disabled": {
-						opacity: .4,
+						opacity: 0.4,
 						cursor: "default"
 					}
 				}),
@@ -106,8 +106,8 @@
 					style: S,
 					onClick: a,
 					type: "button"
-				}, y), x)
-			}
+				}, y), x);
+			};
 		},
 		Br0y: function(e, n, a) {
 			"use strict";
@@ -118,8 +118,8 @@
 				o = a("IP2g"),
 				c = a("8tEE");
 			t.a.createElement;
-			n.a = Object(i.createFragmentContainer)((function(e), {
-				userInfo: void 0 !== l ? l : l = a("/vXk")
+			n.a = Object(i.createFragmentContainer)(function(e), {
+				userInfo: void 0 !== l ? l : l = a("/vXk");
 			})
 		},
 		OohV: function(e, n, a) {
@@ -200,57 +200,6 @@
 					}
 				}
 			}();
-			l.hash = "eizQCoZ-zF3-woGNXWyr6gL1CAoqeiLa", e.exports = l
-		},
-		dmPs: function(e, n, a) {
-			"use strict";
-			a.r(n);
-			var l, r = a("q1tI"),
-				t = a.n(r),
-				s = a("qKvR"),
-				i = a("Ku+5"),
-				o = a("IP2g"),
-				c = a("wHSu"),
-				u = a("P+EW"),
-				d = a("Br0y"),
-				p = a("3LF9"),
-				g = a("nME3"),
-				j = a("PcYu"),
-				m = a("4Us4"),
-				x = a("dOW+"),
-				b = a("6Bx5"),
-				f = a("MfDB"),
-				y = a("0QHm"),
-				v = a("N7W6"),
-				O = a("o2qr"),
-				k = a("x9ul"),
-				h = (t.a.createElement, function() {
-					return Object(s.jsx)(u.a, null, Object(s.jsx)(i.QueryRenderer, {
-						environment: p.a,
-						query: void 0 !== l ? l : l = a("OohV"),
-						render: function(e) {
-							var n, a = e.props;
-							if (!a) return Object(s.jsx)(g.a, {
-								page: !0
-							});
-							var l = a.me;
-							return Object(s.jsx)(t.a.Fragment, null, Object(s.jsx)(d.a, {
-								userInfo: l
-							}), Object(s.jsx)(x.a, null, Object(s.jsx)(b.a, null, Object(s.jsx)("div", {
-								css: {
-									paddingLeft: 25,
-									paddingBottom: 30,
-									paddingRight: 10,
-									"@media(max-width:600px)": {
-										padding: "0px 24px"
-									}
-								}
-							})
-				});
-			n.default = h
 		}
-	},
-	[
-		["3Mmi", 1, 2, 5, 12, 0, 3, 4, 7, 11, 31]
-	]
+	
 ]);
